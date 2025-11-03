c31dd6fd6183b62353bb387a22cc219cbd4054bb
Kaushik Kundu <kaushik.kundu@oracle.com>
2025-11-03 21:29:03 +0530

    Update SQL

---
+++ config/inventory_check2.sql
@@ -0,0 +1,29 @@
+    SELECT
+        SUM(ohd.primary_transaction_quantity) AS available_quantity,
+        item.item_number                      AS item_number,
+        org.organization_code                 AS inv_organization_code,
+        org.organization_id,
+        subinv.secondary_inventory_name       AS secondary_inventory_name,
+        bu.business_unit_name                 AS business_unit_name
+    FROM
+        fdi_idl_catalog.default.dw_inventory_item_d      item          /* Dim_DW_INVENTORY_ITEM_D */,
+        fdi_idl_catalog.default.dw_inv_subinventory_d    subinv        /* Dim_DW_INV_SUBINVENTORY_D */,
+        fdi_idl_catalog.default.dw_internal_org_d                org           /* Dim_DW_INV_ORGANIZATION_D_Inventory_Org */,
+        fdi_idl_catalog.default.dw_inv_onhand_details_cf ohd           /* Fact_DW_INV_ONHAND_DETAILS_CF */,
+        fdi_idl_catalog.default.dw_business_unit_d_tl    bu
+    WHERE subinv.organization_id = ohd.organization_id
+        AND subinv.secondary_inventory_name = ohd.subinventory_code
+        AND org.organization_id = ohd.organization_id
+        AND item.item_number = ?
+        AND bu.business_unit_id = ohd.business_unit_id
+        AND item.organization_id = ohd.organization_id
+        AND org.organization_code      = "002"
+        AND item.inventory_item_id = ohd.inventory_item_id
+        AND org.inv_business_unit_id = bu.business_unit_id
+        AND bu.business_unit_name = "US1 Business Unit"
+    GROUP BY
+        bu.business_unit_name,
+        org.organization_code,
+        subinv.secondary_inventory_name,
+        org.organization_id,
+        item.item_number
--- src/agents/inventory_check_agent.py
+++ src/agents/inventory_check_agent.py
@@ -16,6 +16,8 @@ def inventory_check():
 
     JDBC_DRIVER_PATH = f"{PROJECT_ROOT}/config/SparkJDBC42.jar"
 
+    SQL_PATH = f"{PROJECT_ROOT}/config/inventory_check2.sql"
+
     connection_properties = {
         "oracle.jdbc.authenticationMethod": AUTH_TYPE, 
         "oracle.jdbc.oci.config.file": OCI_CONFIG_FILE,
@@ -34,8 +36,15 @@ def inventory_check():
         cursor = conn.cursor()
 
         # Execute SQL queries
-        cursor.execute("SELECT count(*) FROM fdi_idl_catalog.default.dw_internal_org_d")
+        # cursor.execute("SELECT count(*) FROM fdi_idl_catalog.default.dw_internal_org_d")
         # cursor.execute("SELECT count(*) FROM fdi_idl_catalog.default.dw_inventory_item_d")
+        with open(SQL_PATH, "r") as f:
+            sql_script = f.read()
+        for statement in sql_script.split(';'):
+            if statement.strip(): # Avoid empty statements
+                # cursor.execute(statement)
+                item_number = "AS6647431"
+                cursor.execute(sql_script, (item_number,))
         results = cursor.fetchall()
         print("AIDP Output")
         print(results)
